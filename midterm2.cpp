b68630581ccf065ad4aa409d7d2b1340f17a978f
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-10-17 20:54:08 -0700

    update

--- midterm2.cpp
+++ midterm2.cpp
@@ -128,154 +128,37 @@ size = 0;
         size--;
     }
 
+    vector<string> get_all_customers() { // create a vector to store all customer names
+        vector<string> customers;  // this holds all customer names
+        Node* current = head; // start from the head
+        while (current) { 
+            customers.push_back(current->data); // traverse the list and add each name to the vector
+            current = current->next;    // move to the next node
+        }
+        return customers; // return the vector containing all customer names
+    }
+    void print() {  // function to print the list
+            Node* current = head; // start from the head
+            if (!current) { // if the list is empty
+                cout << "empty" << endl; // print empty message
+                return;
+            }
+            while (current) { // traverse the list
+                cout << "        " << current->data << endl; // print values to current node
+                current = current->next; // move to the next node
+            }
+        }
+        
+        ~DoublyLinkedList() {  // use class destructor to free memory
+            while (head) {  // if there are still nodes in the list
+                Node* temp = head; // store current head
+                head = head->next; // move head to next node
+                delete temp; // free memory for the old head
+            }
+        }
+    };
 
 
-
-
-void insert_after(int value, int position) {
-if (position < 0) {
-cout << "Position must be >= 0." << endl;
-return;
-}
-Node* newNode = new Node(value);
-if (!head) {
-head = tail = newNode;
-return;
-}
-Node* temp = head;
-for (int i = 0; i < position && temp; ++i)
-temp = temp->next;
-if (!temp) {
-cout << "Position exceeds list size. Node not inserted.\n";
-delete newNode;
-return;
-}
-newNode->next = temp->next;
-newNode->prev = temp;
-if (temp->next)
-temp->next->prev = newNode;
-else
-tail = newNode;
-temp->next = newNode;
-}
-void delete_val(int value) {
-if (!head) return;
-Node* temp = head;
-while (temp && temp->data != value)
-temp = temp->next;
-if (!temp) return;
-if (temp->prev)
-temp->prev->next = temp->next;
-else
-head = temp->next;
-if (temp->next)
-temp->next->prev = temp->prev;
-else
-tail = temp->prev;
-delete temp;
-}
-void delete_pos(int pos) {
-if (!head) {
-cout << "List is empty." << endl;
-return;
-}
-if (pos == 1) {
-pop_front();
-return;
-}
-Node* temp = head;
-for (int i = 1; i < pos; i++){
-if (!temp) {
-cout << "Position doesn't exist." << endl;
-return;
-}
-else
-temp = temp->next;
-}
-if (!temp) {
-cout << "Position doesn't exist." << endl;
-return;
-}
-if (!temp->next) {
-pop_back();
-return;
-}
-Node* tempPrev = temp->prev;
-tempPrev->next = temp->next;
-temp->next->prev = tempPrev;
-delete temp;
-}
-
-void push_front(int v) {
-Node* newNode = new Node(v);
-if (!head)
-head = tail = newNode;
-else {
-newNode->next = head;
-head->prev = newNode;
-head = newNode;
-}
-}
-void pop_front() {
-if (!head) {
-cout << "List is empty." << endl;
-return;
-}
-Node * temp = head;
-if (head->next) {
-head = head->next;
-head->prev = nullptr;
-}
-else
-head = tail = nullptr;
-delete temp;
-}
-void pop_back() {
-if (!tail) {
-cout << "List is empty." << endl;
-return;
-}
-Node * temp = tail;
-if (tail->prev) {
-tail = tail->prev;
-tail->next = nullptr;
-}
-else
-head = tail = nullptr;
-delete temp;
-}
-~DoublyLinkedList() {
-while (head) {
-Node* temp = head;
-head = head->next;
-delete temp;
-}
-}
-void print() {
-Node* current = head;
-if (!current) {
-cout << "List is empty." << endl;
-return;
-}
-while (current) {
-cout << current->data << " ";
-current = current->next;
-}
-cout << endl;
-}
-void print_reverse() {
-Node* current = tail;
-if (!current) {
-cout << "List is empty." << endl;
-return;
-}
-while (current) {
-cout << current->data << " ";
-current = current->prev;
-}
-cout << endl;
-}
-};
 int main() {
 cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS; // dummy statement to avoid compiler warning
 
